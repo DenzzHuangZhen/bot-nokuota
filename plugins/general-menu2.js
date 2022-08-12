17758e993ee3e739e75f73d5e6794173bf385557
DenzzHuangZhen <110237780+DenzzHuangZhen@users.noreply.github.com>
2022-08-12 08:49:06 +0700

    Update general-menu2.js

--- plugins/general-menu2.js
+++ plugins/general-menu2.js
@@ -4,7 +4,7 @@ let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
 let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
 conn.sendHydrated(m.chat, '*Xinzuo Bot*', `Hello I'm Xinzuo Bot,YgDi Kembangkan Oleh Denzz,\nꜱɪʟᴀʜᴋᴀɴ ᴋᴇᴛɪᴋ ᴛᴏᴍʙᴏʟ ᴍᴇɴᴜ ᴜɴᴛᴜᴋ ᴍᴇɴᴀᴍᴘɪʟᴋᴀɴ ᴍᴇɴᴜ ʙᴏᴛ\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'bit.ly/Papah-Chan', 'ʏᴏᴜᴛᴜʙᴇ ᴏᴡɴᴇʀ', null, null, [
 [`ᴍᴇɴᴜ`, '.m'],
-[`\nadminya Gey`, 'Bilek']
+[`\nYahaha Kamu Gay`, 'Bilek']
 ], m, {asLocation: true})
 }
 handler.help = ['menu']
